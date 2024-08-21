bd55e354885a93e34e35dbf671b5bb4f6b5cd6ce
thiagojorgelins <tjlh@discente.ifpe.edu.br>
2024-08-21 18:01:38 -0300

    Editar Info

--- src/views/usuario/EditarInformacoes.jsx
+++ src/views/usuario/EditarInformacoes.jsx
@@ -1,59 +1,77 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
-import InputMask from "react-input-mask";
-import { Grid, Button, Container, Divider, Form, FormGroup, FormRadio, FormSelect, Icon, Input, FormTextArea } from 'semantic-ui-react';
-import { notifyError, notifySuccess } from "../util/Util";
-import { getUsername } from "../util/AuthenticationService";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import InputMask from 'react-input-mask';
+import {
+  Grid,
+  Button,
+  Container,
+  Divider,
+  Form,
+  FormGroup,
+  FormRadio,
+  FormSelect,
+  Icon,
+  Input,
+  FormTextArea,
+} from 'semantic-ui-react';
+import { notifyError, notifySuccess } from '../util/Util';
+import { getUsername } from '../util/AuthenticationService';
 
 export default function CadastroUsuario() {
-
   const { state } = useLocation();
   const [idUsuario, setIdUsuario] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmaPassword, setConfirmaPassword] = useState();
   const [nome, setNome] = useState();
   const [sobrenome, setSobrenome] = useState();
 
   useEffect(() => {
-    
-     let username = getUsername();
-     console.log("username é = " + username);
-      axios.get("http://localhost:8080/api/usuario/getUsuario/" + username)
-        .then((response) => {
-          setIdUsuario(response.data.id)
-          setNome(response.data.nome)
-          setSobrenome(response.data.sobrenome)
-
-        })
-    
-  }, [state])
+    let username = getUsername();
+    console.log('username é = ' + username);
+    axios
+      .get('http://localhost:8080/api/usuario/getUsuario/' + username)
+      .then((response) => {
+        setIdUsuario(response.data.id);
+        setEmail(response.data.email);
+        setPassword(response.data.password);
+        setConfirmaPassword(response.data.confirmaPassword);
+        setNome(response.data.nome);
+        setSobrenome(response.data.sobrenome);
+      });
+  }, [state]);
 
   function salvar() {
-
     let usuarioRequest = {
+      email: email,
+      password: password,
+      confirmaPassword: confirmaPassword,
       nome: nome,
-      sobrenome: sobrenome
-    }
-    console.log("Id Usuario = " + idUsuario);
-    if (idUsuario != null) { //Alteração:
+      sobrenome: sobrenome,
+    };
+    console.log('Id Usuario = ' + idUsuario);
+    if (idUsuario != null) {
+      //Alteração:
 
-      axios.put("http://localhost:8080/api/usuario/" + idUsuario, usuarioRequest)
+      axios
+        .put('http://localhost:8080/api/usuario/' + idUsuario, usuarioRequest)
         .then((response) => {
-          console.log('Cliente alterado com sucesso.')
-          notifySuccess('Suas informações foram adicionadas com sucesso!')
+          console.log('Cliente alterado com sucesso.');
+          notifySuccess('Suas informações foram adicionadas com sucesso!');
         })
-        .catch((error) => { console.log('Erro ao alterar suas informações!.')
-          notifyError('Uhmmm! Ocorreu um erro ao salvar suas alterações, tente novamente e se atente aos campos.')
-         }) 
-
+        .catch((error) => {
+          console.log('Erro ao alterar suas informações!.');
+          notifyError(
+            'Uhmmm! Ocorreu um erro ao salvar suas alterações, tente novamente e se atente aos campos.',
+          );
+        });
     }
-    
-} return (
+  }
+  return (
     <div className="corpinhodocadastro">
-
       <div className="containerbranco">
-
-
-        <h2>  Editar Informações </h2>
+        <h2> Editar Informações </h2>
 
         <div className="inputcontainers">
           <div className="input-container">
@@ -62,7 +80,7 @@ export default function CadastroUsuario() {
               fluid
               placeholder
               value={nome}
-              onChange={e => setNome(e.target.value)}
+              onChange={(e) => setNome(e.target.value)}
             />
           </div>
 
@@ -72,32 +90,20 @@ export default function CadastroUsuario() {
               fluid
               placeholder
               value={sobrenome}
-              onChange={e => setSobrenome(e.target.value)}
+              onChange={(e) => setSobrenome(e.target.value)}
             />
           </div>
-
-
-
         </div>
-
 
         <div className="botoesdenaveg">
-        <Button className=""    
-              onClick={() => salvar()}  
-              >
-                Salvar
-    
-              </Button>
+          <Button className="" onClick={() => salvar()}>
+            Salvar
+          </Button>
           <Link to={'/home-usuario'}>
-            <Button className="botaovoltar">
-              Voltar
-            </Button>
+            <Button className="botaovoltar">Voltar</Button>
           </Link>
-
         </div>
-
       </div>
-
     </div>
   );
 }
