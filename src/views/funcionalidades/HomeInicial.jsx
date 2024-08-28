424902099883cd742fe48fa222cd07a5587e8378
juliana <user.taniajuju91@gmail.com>
2024-08-28 10:08:25 -0300

    tela-home

--- src/views/funcionalidades/HomeInicial.jsx
+++ src/views/funcionalidades/HomeInicial.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from 'react-router-dom';
-import { Grid, Button, Container, Divider, Form, FormGroup, FormRadio, FormSelect, Icon, Input } from 'semantic-ui-react';
+import { Grid, Button, Container, Divider, Table, Modal, Header, FormSelect, Icon, Input } from 'semantic-ui-react';
 
 const options = [
     { key: 'editarPerfil', value: 'editarPerfil', text: 'Editar Perfil' },
@@ -12,6 +12,16 @@ export default function HomeInicial() {
 
     const navegando = useNavigate();
     const [idUsuario, setIdUsuario] = useState();
+    const [lista, setLista] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [idRemover, setIdRemover] = useState();
+    const [dataSelecionada, setDataSelecionada] = useState('');
+
+    // Obter datas únicas para o select
+    const datasUnicas = [...new Set(lista.map(pedido => pedido.dataEntrega))];
+
+    // Filtrar os pedidos com base na data selecionada
+    const pedidosFiltrados = lista.filter(pedido => pedido.dataEntrega === dataSelecionada);
 
     const handleChange = (e, { value }) => {
         if (value === 'editarPerfil') {
@@ -21,6 +31,25 @@ export default function HomeInicial() {
         };
 
     }
+
+    useEffect(() => {
+        carregarLista();
+    }, [])
+
+    function carregarLista() {
+
+        axios.get("http://localhost:8080/api/pedido")
+            .then((response) => {
+                setLista(response.data)
+            })
+    }
+
+    function confirmaRemover(id) {
+        setOpenModal(true)
+        setIdRemover(id)
+    }
+
+
 
     return (
 
@@ -41,38 +70,75 @@ export default function HomeInicial() {
                         <Grid.Column>
                             <div className="botoesderota">
 
-                               <Link to={'/Form-Pedidos'}>
-                                <button className="pedidos"
-                                >
-                                    Pedidos
+                                <Link to={'/Form-Pedidos'}>
+                                    <button className="pedidos"
+                                    >
+                                        Pedidos
 
-                                </button>
+                                    </button>
                                 </Link>
 
                                 <Link to={'/List-Materiais'}>
-                                <button className="materiais">
-                                    Materiais
-                                </button>
+                                    <button className="materiais">
+                                        Materiais
+                                    </button>
                                 </Link>
 
                                 <Link to={'/List-Cliente'}>
-                                <button className="clientes">
-                                    Clientes
-                                </button>
+                                    <button className="clientes">
+                                        Clientes
+                                    </button>
                                 </Link>
 
 
                             </div>
                         </Grid.Column>
                         <Grid.Column>
-                            <div className="listadepedidos">
+                            <div className="listaPedidos">
+                                <select className="selectdata" value={dataSelecionada} onChange={(e) => setDataSelecionada(e.target.value)}>
+                                    <option value="">Selecione uma data</option>
+                                    {datasUnicas.map(data => (
+                                        <option key={data} value={data}>{data}</option>
+                                    ))}
+                                </select>
 
+                                <Table color='pink' sortable celled>
+                                    <Table.Header>
+                                        <Table.Row>
+                                            <Table.HeaderCell>Cliente</Table.HeaderCell>
+                                            <Table.HeaderCell>Entrega</Table.HeaderCell>
+                                            <Table.HeaderCell textAlign='center'>Ações</Table.HeaderCell>
+                                        </Table.Row>
+                                    </Table.Header>
+
+                                    <Table.Body>
+                                        {pedidosFiltrados.map(pedido => (
+                                            <Table.Row key={pedido.id}>
+                                                <Table.Cell>{pedido.nomeCliente}</Table.Cell>
+                                                <Table.Cell>{pedido.dataEntrega}</Table.Cell>
+                                                <Table.Cell textAlign='center'>
+                                                    <Button
+                                                        inverted
+                                                        circular
+                                                        color='pink'
+                                                        title='Clique aqui para editar os dados deste material'
+                                                        icon>
+                                                        <Link to="/Form-Pedidos" state={{ id: pedido.id }} style={{ color: 'pink' }}> <Icon name='edit' /> </Link>
+                                                    </Button> &nbsp;
+                                                </Table.Cell>
+                                            </Table.Row>
+                                        ))}
+                                    </Table.Body>
+                                </Table>
                             </div>
                         </Grid.Column>
 
                     </Grid.Row>
                 </Grid>
             </div>
+
+
+
         </div>
 
 
